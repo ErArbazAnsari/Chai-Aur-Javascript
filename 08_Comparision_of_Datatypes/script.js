de42c8799c11dbd985a32daddb88b1ef904d9078
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-30 23:24:59 +0530

    comparision between different different datatypes.

---
+++ 08_Comparision_of_Datatypes/script.js
@@ -0,0 +1,32 @@
+// comparison in javascript
+// let a = 10
+// let b = 20
+// console.log(a > b)
+// console.log(a >= b)
+// console.log(a < b)
+// console.log(a <= b)
+// console.log(a == b)
+// console.log(a != b)
+
+
+// console.log("1" >= 1)
+// console.log("01" >= 1)
+
+
+// console.log(null == 0)
+// console.log(null > 0)
+// console.log(null < 0)
+// console.log(null >= 0)
+// console.log(null <= 0)
+// console.log(typeof null)
+// console.log(typeof undefined)
+
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+
+// Strict Check
+// console.log(10 === "a")
+
