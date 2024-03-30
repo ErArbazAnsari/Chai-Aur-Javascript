ac7aa0f1687b49a233adc719bb161a36d6be708a
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-30 23:07:13 +0530

    string to number conversion

---
+++ 07_String_to_number_conversion/script.js
@@ -0,0 +1,25 @@
+let value = 3
+
+let negativeVal = -value
+console.log(negativeVal)
+
+let str1 = "Arbaz"
+let str2 = "Ansari"
+console.log(str1 + str2) //string concatenation
+
+console.log("1" + 2)
+console.log(1 + "2")
+console.log("1" + 2 + 2 + 2) //1222
+console.log(1 + 2 + "2") //32
+
+
+console.log(true) //true
+console.log(+true) //1
+// console.log(true+) //This stmt provide error.
+
+console.log(+"") //0
+
+let counter = 100;
+console.log(counter++) //101
+console.log(++counter) //102
+
