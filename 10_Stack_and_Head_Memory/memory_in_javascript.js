bd5f6ef8eb69ebb57f746eff1cf87e1734671f68
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-31 00:14:27 +0530

    10_Stack_and_Head_Memory

---
+++ 10_Stack_and_Head_Memory/memory_in_javascript.js
@@ -0,0 +1,18 @@
+// We have to types of memory
+// 1. stack
+// 2. heap
+
+let myYoutubename = "hiteshchoudharydotcom"
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google. com",
+    upi: "user@ybl"
+}
+let userTwo = userOne
+userTwo.email = "hitesh@google. com"
+console.log(userOne.email)
+console.log(userTwo.emaitv)
