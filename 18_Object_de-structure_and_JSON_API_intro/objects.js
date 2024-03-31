807c597d0b8df083255fd5851f411b60444810e8
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-31 18:15:28 +0530

    objects destructuring

---
+++ 18_Object_de-structure_and_JSON_API_intro/objects.js
@@ -0,0 +1,25 @@
+//Object de-structure and JSON API intro
+
+const courses = {
+    courseName: "Learn Javascript",
+    price: "Free",
+    instructor: "Hitesh Choudhary"
+}
+// console.log(courses)
+// console.log("\nCourse Name:", courses.courseName)
+// console.log("Course Price:", courses.price)
+// console.log("Course Instructor:", courses.instructor, "\n")
+
+//Another Way to print
+// const { courseName: cn, price, instructor } = courses
+
+// console.log(cn)
+// console.log(price)
+// console.log(instructor)
+
+//JSON
+// {
+//     "name": "Arbaz Ansari",
+//     "age": 20,
+//     "favLanguage": "Javascript"
+// }
