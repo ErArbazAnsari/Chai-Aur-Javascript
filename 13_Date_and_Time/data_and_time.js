fa0247fd6916c190c47b0e28945f1b72f5cde92b
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-31 11:33:56 +0530

    date and time in javascript

---
+++ 13_Date_and_Time/data_and_time.js
@@ -0,0 +1,42 @@
+//Dates
+
+let mydate = new Date()
+// console.log(mydate)
+// console.log(mydate.toString())
+// console.log(mydate.toISOString())
+// console.log(mydate.toDateString())
+// console.log(mydate.toTimeString())
+// console.log(mydate.toJSON())
+// console.log(mydate.toLocaleDateString())
+// console.log(mydate.toTimeString())
+// console.log(mydate.getTimezoneOffset())
+// console.log(typeof mydate)
+
+// let newdate = new Date(2023, 11, 20)
+// console.log(newdate.toDateString())
+
+// let oneday = new Date(2023, 11, 20, 5, 3)
+// console.log(oneday.toLocaleString())
+
+// let oneday = new Date("2023-01-24")
+// console.log(oneday.toLocaleString())
+let oneday = new Date("01-26-2024")
+// console.log(oneday.toLocaleString())
+
+
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+// console.log(oneday.getTime())
+
+let newDate = new Date()
+console.log(newDate)
+console.log(newDate.getDate())
+console.log(newDate.getMonth())
+console.log(newDate.getTime())
+console.log(newDate.getDay())
+console.log(newDate.getHours())
+console.log(newDate.getFullYear())
+console.log(newDate.getMilliseconds())
+console.log(newDate.getMinutes())
+console.log(newDate.getSeconds())
