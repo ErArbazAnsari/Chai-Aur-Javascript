5fec6d0a041605e0fa4026789b55b203f91e77f8
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-03-31 18:57:51 +0530

    fuction with objects and arrays

--- 20_Function_with_Objects_and_Array/script.js
+++ 20_Function_with_Objects_and_Array/script.js
@@ -1,2 +1,35 @@
 // Function with objects and array
 
+//Rest Operator
+function countCart(...num) {
+    return num
+}
+console.log(countCart(1, 2, 3, 4, 5))
+
+
+// objects
+const myObj = {
+    name: "Arbaz",
+    age: 20
+}
+
+function myFunction(anyObject) {
+    return `Name is: ${myObj.name} and age: ${myObj.age}`
+}
+
+console.log(myFunction(myObj))
+console.log({
+    name: "Arbaz Ansari",
+    age: 100
+})
+
+
+const myArray = [1, 2, 3, 4, 5,]
+function myArrayFunction(getArray, index) {
+    return getArray[index]
+}
+
+console.log(myArrayFunction(myArray, 1))
+console.log(myArrayFunction([
+    10, 20, 30, 40, 100
+], 1))
