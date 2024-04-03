2349a9b12ed5343b1385c5393db99dc203bc22bc
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-04-03 20:30:12 +0530

    01_Contro_Flow

---
+++ 26_Control_Flow_in_JS/01_Control_Flow.js
@@ -0,0 +1,57 @@
+// if statements
+
+// if(true){
+//     // statements
+//     console.log("JavaScript")
+// }
+
+// if(false){
+//     // statements
+//     console.log("JavaScript")
+// }
+
+
+let isUserLonggined = false
+
+if (isUserLonggined) {
+    // console.log("Yes, User is Loggined.")
+}
+
+// <, >, <=, >=, ==, !=, ===, !==
+if (3 != 2) {
+    // console.log("3 is not equal to 2");
+}
+
+if (3 === '3') {
+    // console.log("Exactly Equal")
+}
+
+
+if (2 !== '2') {
+    // console.log("Not Equal Value");
+}
+
+
+let temperature = 141;
+if (temperature < 50) {
+    // console.log("Temperature is Less than 50")
+}
+else {
+    // console.log("Temperature is Higher than 50")
+}
+
+
+
+// let score = 100;
+// if (score > 150) {
+//     let newScore = 200;
+//     console.log("Old score:", score);
+//     console.log("New score:", newScore);
+// }
+// console.log(newScore)// newScore is not defined
+// console.log(score)
+
+
+let score = 200;
+if (score > 400) console.log("Score is high")
+else console.log("Less Score")
