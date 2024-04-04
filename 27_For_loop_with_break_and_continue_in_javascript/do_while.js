994d70b63517a1b9a799d7e9e8f46f6c1f14284f
Arbaz Ansari <mail.arbazansari@gmail.com>
2024-04-04 10:09:22 +0530

    while do , do while loop in js

---
+++ 27_For_loop_with_break_and_continue_in_javascript/do_while.js
@@ -0,0 +1,6 @@
+// do while loop in javascript
+let arr = 11
+do {
+    console.log(`value is: ${arr}`)
+    arr++
+} while (arr < 10);
--- 27_For_loop_with_break_and_continue_in_javascript/while_loop.js
+++ 27_For_loop_with_break_and_continue_in_javascript/while_loop.js
@@ -1,2 +1,14 @@
 // while loop in javascript
 
+let index = 0
+while (index < 100) {
+    // console.log(`index value ${index}`)
+    index += 2;
+}
+
+let myArray = [1, 2, 3, 4, 5]
+let arr = 0;
+while (arr < myArray.length) {
+    console.log(`value is: ${myArray[arr]}`)
+    arr++;
+}
